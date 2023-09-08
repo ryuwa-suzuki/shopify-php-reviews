b2e88a1fa1c27f227648fc6717b0281bb6966299
ryuwa <ryuusuzuki243@gmail.com>
2023-09-08 16:06:24 +0900

    add: sessionの登録

--- web/app/Models/Session.php
+++ web/app/Models/Session.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Shopify\Auth\Session as AuthSession;
 
 class Session extends Model
 {
     use HasFactory;
+
+    public function instantiateAuthSession()
+    {
+        $authSession = new AuthSession(
+            $this->id,
+            $this->shop,
+            $this->is_online,
+            $this->state
+        );
+        $authSession->setAccessToken($this->access_token);
+
+        return $authSession;
+    }
 }
